3f63fa62e1f0b544bbaa072c298a88970b72e52e
RoteGer <rotem.ger207@gmail.com>
2023-08-29 12:49:26 +0300

    Start of Gui

--- src/il/ac/hit/quizzy/GuiQuiz.java
+++ src/il/ac/hit/quizzy/GuiQuiz.java
@@ -1,20 +1,35 @@
 package il.ac.hit.quizzy;
+import javax.swing.*;
+import java.awt.*;
 
 public class GuiQuiz implements IQuiz {
     // Implementation for GUI-based quiz
+    private String name;
+    private JFrame frame; // The main GUI frame
+    private JLabel gradeLabel; // A label to display the final grade
+
     @Override
     public void start() {
+        // Create the main frame
+        frame = new JFrame("Quiz App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600); // Set the dimensions of the frame
+        // Initialize and add GUI components (questions, buttons, etc.)
+        // You would need to create GUI components and add them to the frame here
 
+        // Display the frame
+        frame.setVisible(true);
     }
 
     @Override
     public void setName(String text) {
-
+        this.name = text;
     }
+
 
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
@@ -24,13 +39,33 @@ public class GuiQuiz implements IQuiz {
 
     @Override
     public void endQuiz() {
+        // Calculate the final grade
+        int finalGrade = calculateFinalGrade();
 
+        // Create a label to display the grade
+        gradeLabel = new JLabel("Final Grade: " + finalGrade);
+        gradeLabel.setFont(new Font("Arial", Font.BOLD, 24)); // Set font and size
+
+        // Add the label to the frame
+        frame.getContentPane().removeAll(); // Remove previous components
+        frame.getContentPane().add(gradeLabel, BorderLayout.CENTER);
+
+        // Update the frame
+        frame.revalidate();
+        frame.repaint();
+    }
+
+    private int calculateFinalGrade() {
+        return 1;
     }
 
     @Override
     public IQuiz clone() {
-        // Implement the clone logic for GUI
-        // Return a new instance with copied properties
-        return null;
+        GuiQuiz clonedQuiz = new GuiQuiz();
+        // Copy properties from the current instance to the cloned instance
+        clonedQuiz.name = this.name;
+        // You might need to copy other GUI-related properties here
+
+        return clonedQuiz;
     }
 }
