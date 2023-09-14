51e3e9f192c931ea873ddce14cdc7f0ee41af512
RoteGer <rotem.ger207@gmail.com>
2023-09-14 22:00:51 +0300

    GUIQuiz Unit Tests - finished

--- tests/il/ac/hit/quizzy/tests/GUIQuizTest.java
+++ tests/il/ac/hit/quizzy/tests/GUIQuizTest.java
@@ -1,38 +1,71 @@
 package il.ac.hit.quizzy.tests;
 
+import il.ac.hit.quizzy.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+// setName, getName, addQuestion, clone
 public class GUIQuizTest {
 
+    private IQuiz guiQuiz;
 
+    @Before
+    public void setUp() {
+        // Create the QuizFactory and the GUIQuiz instance
+        QuizFactory factory = new QuizFactory();
+        guiQuiz = factory.createQuiz(QuizType.GUI);
+        guiQuiz.setName("Quiz Test");
 
-    @org.junit.Before
-    public void setUp() throws Exception {
+        // Create a sample question and add it to the guiQuiz
+        IQuizQuestionBuilder builderTest = new QuizQuestion.Builder();
+        builderTest.setTitle("We Love Canada");
+        builderTest.setQuestion("Canada starts with…?");
+        builderTest.addAnswer("Canada starts with the letter ‘A’.", false);
+        builderTest.addAnswer("Canada starts with the letter ‘B’.", false);
+        builderTest.addAnswer("Canada starts with the letter ‘C’.", true);
+        builderTest.addAnswer("Canada starts with the letter ‘D’.", false);
+        builderTest.addAnswer("Canada starts with the letter ‘E’.", false);
+        IQuizQuestion questionTest = builderTest.create();
+        guiQuiz.addQuestion(questionTest);
     }
 
-    @org.junit.After
-    public void tearDown() throws Exception {
+    @Test
+    public void testSetName() {
+
+        // Check when name is not sent
+        guiQuiz.setName(null);
+        assertEquals("Default Name", guiQuiz.getName());
+
+        // Check when name is set
+        guiQuiz.setName("TestQuizName");
+        assertEquals("TestQuizName", guiQuiz.getName());
     }
 
-    @org.junit.Test
-    public void setName() {
+    @Test
+    public void testGetName() {
+
+        // "Quiz Test" was set in the setUp method
+        assertEquals("Quiz Test", guiQuiz.getName());
     }
 
-    @org.junit.Test
-    public void getName() {
+    @Test
+    public void testAddQuestion() {
+
+        // Question was added in the setUp method
+        assertEquals(1, guiQuiz.getQuestions().size());
     }
 
-    @org.junit.Test
-    public void getQuestions() {
-    }
+    @Test
+    public void testClone() throws CloneNotSupportedException {
+        GUIQuiz clonedQuiz = (GUIQuiz) guiQuiz.clone();
 
-    @org.junit.Test
-    public void addQuestion() {
-    }
+        // Check that the cloned quiz is a different object
+        assert (guiQuiz != clonedQuiz);
 
-    @org.junit.Test
-    public void endQuiz() {
-    }
-
-    @org.junit.Test
-    public void testClone() {
+        // Check that the questions are cloned correctly
+        assertEquals(guiQuiz.getQuestions().size(), clonedQuiz.getQuestions().size());
+        assertEquals(guiQuiz.getQuestions().get(0).getQuestion(), clonedQuiz.getQuestions().get(0).getQuestion());
     }
 }
