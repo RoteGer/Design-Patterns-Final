7de7a25c394708c9e2457937f73d55708fa37d85
RoteGer <rotem.ger207@gmail.com>
2023-09-14 18:00:24 +0300

    Start of unit tests junit

---
+++ tests/il/ac/hit/quizzy/tests/GUIQuizTest.java
@@ -0,0 +1,38 @@
+package il.ac.hit.quizzy.tests;
+
+public class GUIQuizTest {
+
+
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setName() {
+    }
+
+    @org.junit.Test
+    public void getName() {
+    }
+
+    @org.junit.Test
+    public void getQuestions() {
+    }
+
+    @org.junit.Test
+    public void addQuestion() {
+    }
+
+    @org.junit.Test
+    public void endQuiz() {
+    }
+
+    @org.junit.Test
+    public void testClone() {
+    }
+}
